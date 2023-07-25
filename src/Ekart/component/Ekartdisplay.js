131cc8114d47693e7466d4f5500f37827d4345db
karthik <karthickrl@outlook.com>
2023-07-25 22:21:17 +0530

    25july commit

--- src/Ekart/component/Ekartdisplay.js
+++ src/Ekart/component/Ekartdisplay.js
@@ -1,81 +1,71 @@
 /* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import SweetPagination from "sweetpagination";
-import { AiOutlineShareAlt } from 'react-icons/ai';
-import { RiHeartAddFill } from 'react-icons/ri';
-import { BiExpand } from 'react-icons/bi';
+import { AiOutlineShareAlt } from "react-icons/ai";
+import { RiHeartAddFill } from "react-icons/ri";
+import { BiExpand } from "react-icons/bi";
 
 function Ekartdisplay(props) {
-    const products = props.Content
-    const categories = props.categories
-    var CartItems = props.cartelement
-    var wishlistitem=props.wishlistelement
+  const products = props.Content;
+  const categories = props.categories;
+  var CartItems = props.cartelement;
+  var wishlistitem = props.wishlistelement;
+  const [count, setcount] = useState([]);
 
-    const [count, setcount] = useState([]);
-    
-
-
-    const Addtocart = async (e) => {
-        var arr = products.filter(products => products.id === e)
-        var arr2 = CartItems.filter(content => content[0].id === e)
-        arr.qty = 1
-        var Cartvar = CartItems
-        if(arr2.length == 0){
-            Cartvar.push(arr)
-        }
-
-        props.changecartcount(CartItems.length)
-        props.cartelements(CartItems)
-
+  const Addtocart = async (e) => {
+    var arr = products.filter((products) => products.id === e);
+    var arr2 = CartItems.filter((content) => content[0].id === e);
+    arr.qty = 1;
+    var Cartvar = CartItems;
+    if (arr2.length == 0) {
+      Cartvar.push(arr);
     }
 
-    const Addtowhislist = async (e) => {
+    props.changecartcount(CartItems.length);
+    props.cartelements(CartItems);
+  };
 
-        var arr = products.filter(products => products.id === e)
-        var arr2 = wishlistitem.filter(content => content[0].id === e)
-        arr.qty = 1
-        var Cartvar = wishlistitem
-        if(arr2.length == 0){
-            Cartvar.push(arr)
-        }
-
-        props.changewhishlistcount(wishlistitem.length)
-        props.wishlistelements(wishlistitem)
-
+  const Addtowhislist = async (e) => {
+    var arr = products.filter((products) => products.id === e);
+    var arr2 = wishlistitem.filter((content) => content[0].id === e);
+    arr.qty = 1;
+    var Cartvar = wishlistitem;
+    if (arr2.length == 0) {
+      Cartvar.push(arr);
     }
 
-    
+    props.changewhishlistcount(wishlistitem.length);
+    props.wishlistelements(wishlistitem);
+  };
 
-    function checkIfDuplicateExists(arr) {
-        return arr.filter((item, index) => arr.indexOf(item) === index);
-    }
-    
-    return (
-        <div className='mt'>
-            <div className="sidebar gt">
-                <h1>Catageories</h1>
-                <a className="active" onClick={() => props.changecategorie(' ')}>All</a>
-                {
-                    categories.map((cat) => {
-                        return (<><a className="active" onClick={() => props.changecategorie(cat)}>{cat}</a></>)
-                    })
+  return (
+    <div className="mt">
+      <div className="sidebar gt">
+        <h1>Catageories</h1>
+        <a className="active" onClick={() => props.changecategorie(" ")}>
+          All
+        </a>
+        {categories.map((cat) => {
+          return (
+            <>
+              <a className="active" onClick={() => props.changecategorie(cat)}>
+                {cat}
+              </a>
+            </>
+          );
+        })}
+      </div>
 
-                }
-            </div>
-
-            <div className='content'>
-                {products.length > 0
-                    ?
-                    <div className="row">
-
-                        {
-                            count.map((val) => {
-                                return (
-                                    <>
-                                        {/* 
+      <div className="content">
+        {products.length > 0 ? (
+          <div className="row">
+            {count.map((val) => {
+              return (
+                <>
+                  {/* 
                                         <div className="col-sm-4">
                                             <div className="card my-3 rounded-bottom">
                                                 <img className="card-img-top height" src={val.thumbnail}></img>
@@ -89,46 +79,62 @@ function Ekartdisplay(props) {
                                                 </div>
                                             </div>
                                         </div> */}
-                                        {/* Single Product */}
-                                        <div class=" col-lg-4 col-xl-3 ">
-                                            <div id="product-2" class="single-product" >
-                                                <div class="part-1" >
-                                                    <img src={val.thumbnail}></img>
-                                                    <span class="discount">{val.discountPercentage}%</span>
-                                                    {/* <span class="new">{val.discountPercentage}%</span> */}
-                                                    <ul>
-                                                        <li><a ><AiOutlineShareAlt /></a></li>
-                                                        <li><a onClick={() => Addtowhislist(val.id)}><RiHeartAddFill /></a></li>
-                                                        <li><a ><BiExpand /></a></li>
-                                                    </ul>
-                                                </div>
-                                                <div class="part-2">
-                                                    <h3 class="product-title">{val.title}</h3>
-                                                    <h4 class="product-price">RS.{val.price}</h4>
-                                                    <h4 class=" product-stock">Stock:{val.stock}</h4>
-                                                    <button className="btn btn-light"  onClick={() => Addtocart(val.id)}>ADD TO CART</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </>)
-                            })}
-                        <SweetPagination
-                            currentPageData={setcount}
-                            dataPerPage={12}
-                            getData={products}
-                            navigation={true}
-                        />
+                  {/* Single Product */}
+                  <div class=" col-lg-4 col-xl-3 ">
+                    <div id="product-2" class="single-product">
+                      <div class="part-1">
+                        <img src={val.thumbnail}></img>
+                        <span class="discount">{val.discountPercentage}%</span>
+                        {/* <span class="new">{val.discountPercentage}%</span> */}
+                        <ul>
+                          <li>
+                            <a>
+                              <AiOutlineShareAlt />
+                            </a>
+                          </li>
+                          <li>
+                            <a onClick={() => Addtowhislist(val.id)}>
+                              <RiHeartAddFill />
+                            </a>
+                          </li>
+                          <li>
+                            <a>
+                              <BiExpand />
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="part-2">
+                        <h3 class="product-title">{val.title}</h3>
+                        <h4 class="product-price">RS.{val.price}</h4>
+                        <h4 class=" product-stock">Stock:{val.stock}</h4>
+                        <button
+                          className="btn btn-light"
+                          onClick={() => Addtocart(val.id)}
+                        >
+                          ADD TO CART
+                        </button>
+                      </div>
                     </div>
-                    :
-                    <div class="text-center mt-4">
-                        
-                        <h1>Loading ......</h1>
-                    </div>
-                }
-
-            </div>
-
-        </div>);
+                  </div>
+                </>
+              );
+            })}
+            <SweetPagination
+              currentPageData={setcount}
+              dataPerPage={12}
+              getData={products}
+              navigation={true}
+            />
+          </div>
+        ) : (
+          <div class="text-center mt-4">
+            <h1>Loading ......</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Ekartdisplay;
